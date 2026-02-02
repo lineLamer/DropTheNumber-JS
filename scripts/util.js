f9f701be61cc780a95560e9053aca8cd6ab92961
line Lamer <103029871+lineLamer@users.noreply.github.com>
2026-02-02 03:40:45 +0100

    Update util.js
    Removed commented-out color and number entries to streamline the code.

--- scripts/util.js
+++ scripts/util.js
@@ -6,12 +6,7 @@ export function getRandomRect() {
         { color: "#393cf0", number: "16" },
         { color: "#3133c0", number: "32" },
         { color: "#0e11af", number: "64" },
-        { color: "#02047d", number: "128" }//,
-        //{ color: "#1d1d44", number: "256" },
-        //{ color: "#5c8dcc", number: "512" },
-        //{ color: "#179ee1", number: "1024" },
-        //{ color: "#073850", number: "2048" }//,
-        //{ color: "#1398a1", number: "4096" }
+        { color: "#02047d", number: "128" }
     ];
 
     let totalProbability = rectObject.reduce((sum, rect) => sum + (rect.probability || 1), 0);
